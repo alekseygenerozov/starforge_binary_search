b64493b5cc54aef0f806eddc5f829a45f160e571
Aleksey Generozov <aleksey.generozov@gmail.com>
2023-05-25 15:01:45 +0300

    Refactoring halo mass code.

--- halo_masses_single_double.py
+++ halo_masses_single_double.py
@@ -36,55 +36,11 @@ def KE(xc, mc, vc, uc):
     return (mc * (vSqr / 2 + uc)).sum()
 
 
-# @njit('float64(float64[:], float64[:], float64[:], float64[:], float64, float64, float64)')
-def get_sma_opt(p1, p2, v1, v2, m1, m2, h1, h2, u1, u2, G):
-    """        # ke1 = KE(np.vstack([com_pos, xuniq[idx]]), np.append(com_masses, muniq[idx]),\
-        #          np.vstack([com_vel, vuniq[idx]]), np.append(0, uuniq[idx]))
-    Auxiliary function to get binary properties for two particles.
 
-    :param Array-like p1: -- Array with 1st particle position (3D)
-    :param Array-like p2: -- Array with 2nd particle position (3D)
-    :param Array-like v1: -- Array with 1st particle velocity (3D)
-    :param Array-like v2: -- Array with 2nd particle velocity (3D)
-    :param float m1: -- 1st particle mass
-    :param float m2: -- 2nd particle mass
-    :param float h1: -- 1st particle softening length
-    :param float h2: -- 2nd particle softening length
-    :param float u1: -- 1st particle internal energy
-    :param float u2: -- 2nd particle internal energy
-
-    :return: The semimajor axis, eccentricity, inclination, the particle separation, com position, com velocity, m1, and m2.
-    :rtype: tuple
+def check_tides_gen(pos, mass, accel, soft, idx1, idx2, G, compress=False, tides_factor=1):
     """
-    dp = np.linalg.norm(p1 - p2)
-
-    com = (m1 * p1 + m2 * p2) / (m1 + m2)
-    com_vel = (m1 * v1 + m2 * v2) / (m1 + m2)
-    ##Particle velocities in com frame
-    p1_com = p1 - com
-    p2_com = p2 - com
-    v1_com = v1 - com_vel
-    v2_com = v2 - com_vel
-
-    v12 = (v1_com[0] ** 2. + v1_com[1] ** 2. + v1_com[2] ** 2.)
-    v22 = (v2_com[0] ** 2. + v2_com[1] ** 2. + v2_com[2] ** 2.)
-
-    ##Kinetic and potential energies (In principle for gas should also include internal energy)
-    #     ke = 0.5*m1*v12 + 0.5*m2*v22
-    ke = KE(np.array([p1_com, p2_com]), np.array([m1, m2]), np.array([v1_com, v2_com]), np.array([u1, u2]))
-    ##Potential energy;
-    # pe = G*m1*m2/dp
-    pe = -PE(np.array([p1_com, p2_com]), np.array([m1, m2]), np.array([h1, h2]), G=G)
-    a_bin = G * (m1 * m2) / (2. * (pe - ke))
-
-    return a_bin
-
-
-# Estimate of tidal force for SINGLE gas cell + multiple...position, mass, etc. of gas cell
-# has to come first in pos, mass, etc.
-def check_tides_gen(pos, mass, accel, soft, idx1, idx2, G):
-    """
-    Check whether tidal force is greater than two-body force between two stars.
+    Estimate of tidal force for SINGLE gas cell + multiple...position, mass, etc. of gas cell
+    has to come first in pos, mass, etc.
 
     :param Array-like mass: Particle positions
     :param Array-like pos: Particle positions
@@ -92,6 +48,9 @@ def check_tides_gen(pos, mass, accel, soft, idx1, idx2, G):
     :param Array-like soft: Softening length
     :param int idx1: First particle index
     :param int idx2: Second particle index
+    :param float G: Gravitational constant
+    :param bool compress (False): Filtering out compressive tides
+    :param float tides_factor (1): Prefactor used in comparison of tidal and internal forces.
 
     :return: Boolean indicating whether tidal force exceed the internal two-body force.
     :rtype: bool
@@ -102,13 +61,126 @@ def check_tides_gen(pos, mass, accel, soft, idx1, idx2, G):
                                                softening_target=np.atleast_1d(soft[idx1]),
                                                softening_source=np.atleast_1d(soft[idx2]), G=G)
     com_accel = (mass[idx1] * accel[idx1] + np.sum(mass[idx2]) * accel[idx2]) / (mass[idx1] + np.sum(mass[idx2]))
+    ##Safeguard for rounding error??
     f_tides = mass[idx1] * (accel[idx1] - com_accel) - f2body_i
 
-    tidal_crit = (np.linalg.norm(f_tides) < np.linalg.norm(f2body_i))
+    tidal_crit = (np.linalg.norm(f_tides) < tides_factor * np.linalg.norm(f2body_i))
+    ##Add compressive criterion HERE!!
+    com_pos = np.average(pos[idx2], weights=mass[idx2], axis=0)
+    if compress:
+        compress_check = np.dot(f_tides, com_pos - pos[idx1]) > 0
+        tidal_crit = tidal_crit or compress_check
+
+
     return tidal_crit
 
-##Add cumulative argument for non-pairwise algorithm!!!
-def get_gas_mass_bound(sys1, xuniq, muniq, huniq, accel_gas, G=GN, cutoff=0.1, non_pair=False):
+def blob_setup(sys1):
+    """
+    Bookkeeping function for get_gas_mass_bound
+    """
+    cumul_masses = np.array([sys1.mass])
+    cumul_soft = np.array([sys1.soft])
+
+    cumul_pos = np.copy([sys1.pos])
+    cumul_vel = np.copy([sys1.vel])
+    cumul_accel = np.copy(sys1.accel)
+
+    cumul_u = np.zeros(len(cumul_pos))
+
+    com_masses = sys1.mass
+    com_pos = np.copy(sys1.pos)
+    com_vel = np.copy(sys1.vel)
+
+    blob = {'cumul_masses': cumul_masses,
+            'cumul_pos': cumul_pos,
+            'cumul_soft': cumul_soft,
+            'cumul_vel': cumul_vel,
+            'cumul_u': cumul_u,
+            'cumul_accel': cumul_accel,
+            'com_masses': com_masses,
+            'com_pos': com_pos,
+            'com_vel': com_vel}
+
+    return blob
+
+def add_to_blob(blob, gas_data, idx):
+    """
+    Bookkeeping function for get_gas_mass_bound
+    """
+    ##Could make copies but may take up too much memory...
+    xuniq1, vuniq1, muniq1, huniq1, uuniq1, accel_gas1 = gas_data
+
+    ##This is really the com_accel: Rename to keep the pattern
+    blob['cumul_accel'] = (blob['com_masses'] * blob['cumul_accel'] + muniq1[idx] * accel_gas1[idx]) /\
+                          (muniq1[idx] + blob['com_masses'])
+    blob['cumul_masses'] = np.append(blob['cumul_masses'], muniq1[idx])
+    blob['cumul_u'] = np.append(blob['cumul_u'], uuniq1[idx])
+    blob['cumul_pos'] = np.vstack([blob['cumul_pos'], xuniq1[idx]])
+    blob['cumul_vel'] = np.vstack([blob['cumul_vel'], vuniq1[idx]])
+    blob['cumul_soft'] = np.append(blob['cumul_soft'], huniq1[idx])
+    blob['com_masses'] = np.sum(blob['cumul_masses'])
+    blob['com_pos'] = np.average(blob['cumul_pos'], weights=blob['cumul_masses'], axis=0)
+    blob['com_vel'] = np.average(blob['cumul_vel'], weights=blob['cumul_masses'], axis=0)
+
+    return blob
+
+def get_gas_mass_bound_refactor(sys1, gas_data, G=GN, cutoff=0.1, non_pair=False, compress=False, tides_factor=1):
+    """
+    Get to gas mass bound to a system. This is meant to be applied to a *single star.*
+
+    :param System sys1: System we are interested
+    :param tuple gas_data: All the positions, velocities, masses, softening lengths, and accelerations of all gas
+    :param float G: Gravitational constant
+    :param float cutoff: Distance up to which we look for bound gas
+    :param bool non_pair: Flag to include non-pairwise interactions.
+    """
+    blob = blob_setup(sys1)
+    xuniq1, vuniq1, muniq1, huniq1, uuniq1, accel_gas1 = gas_data
+
+    d = xuniq1 - blob['com_pos']
+    d = np.sum(d * d, axis=1)**.5
+    ord1 = np.argsort(d)
+    d_max = 0
+    halo_mass = 0.
+    bound_index = []
+    particle_indices = range(len(xuniq1))
+    for idx in progressbar.progressbar(ord1):
+        if d[idx] > cutoff:
+            break
+        ##Use velocity relative to the cumulative center-of-mass
+        tmp_vrel = np.linalg.norm(vuniq1[idx] - blob['com_vel'])
+        ##Performance but unexpected decreases bound gas
+        if tmp_vrel > np.sqrt((2. * G * (blob['com_masses'] + muniq1[idx])) / d[idx]):
+            continue
+
+        pe1 = muniq1[idx] * pytreegrav.PotentialTarget(np.atleast_2d(xuniq1[idx]), blob['cumul_pos'],
+                                                blob['cumul_masses'],
+                                                softening_target=np.atleast_1d(huniq1[idx]),
+                                                softening_source=blob['cumul_soft'],
+                                                G=G, method='bruteforce')[-1]
+        ke1 = KE(np.vstack([blob['com_pos'], xuniq1[idx]]), np.append(blob['com_masses'], muniq1[idx]),
+                 np.vstack([blob['com_vel'], vuniq1[idx]]), np.append(0, uuniq1[idx]))
+
+        ##Could refactor this part
+        tmp_pos = [xuniq1[idx], blob['cumul_pos']]
+        tmp_mass = [muniq1[idx], blob['cumul_masses']]
+        tmp_soft = [huniq1[idx], blob['cumul_soft']]
+        tmp_accel = [accel_gas1[idx], blob['cumul_accel']]
+        tide_crit = check_tides_gen(tmp_pos, tmp_mass, tmp_accel, tmp_soft, 0, 1, G, compress=compress,
+                                    tides_factor=tides_factor)
+
+        if (pe1 + ke1 < 0) and (tide_crit):
+            if non_pair:
+                blob = add_to_blob(blob, gas_data, idx)
+
+            d_max = d[idx]
+            halo_mass += muniq1[idx]
+            bound_index.append(particle_indices[idx])
+
+    return halo_mass, d_max, bound_index
+
+#Add cumulative argument for non-pairwise algorithm!!! Add vuniq to arguments as well
+def get_gas_mass_bound_og(sys1, xuniq, muniq, huniq, accel_gas, G=GN, cutoff=0.1, non_pair=False):
     if sys1.multiplicity > 1:
         cumul_masses = np.copy(sys1.sub_mass)
         cumul_pos = np.copy(sys1.sub_pos)
@@ -137,18 +209,18 @@ def get_gas_mass_bound(sys1, xuniq, muniq, huniq, accel_gas, G=GN, cutoff=0.1, n
         if d[idx]**.5 > cutoff:
             break
         ##Likely unnecessary since no change in results/performance.
-        if denuniq[idx] < 0.247:
-            continue
+        # if denuniq[idx] < 0.247:
+        #     continue
         ##Use velocity relative to the cumulative center-of-mass
         tmp_vrel = np.linalg.norm(vuniq[idx] - com_vel)
-        if tmp_vrel > (2. * G * np.sum(cumul_masses) / d[idx]**.5)**.5:
+        if tmp_vrel > (2. * G * (com_masses + muniq[idx]) / d[idx]**.5)**.5:
             continue
 
         ##Maybe can also replace with PotentialTarget?! Gives a lot of extra information...
-        pe0 = muniq[idx] * pytreegrav.Potential(np.vstack((cumul_pos, xuniq[idx])),
-                                                np.append(cumul_masses, muniq[idx]),
-                                                softening=np.append(cumul_soft, huniq[idx]),
-                                                G=G, theta=0.7, method='bruteforce')
+        # pe0 = muniq[idx] * pytreegrav.Potential(np.vstack((cumul_pos, xuniq[idx])),
+        #                                         np.append(cumul_masses, muniq[idx]),
+        #                                         softening=np.append(cumul_soft, huniq[idx]),
+        #                                         G=G, theta=0.7, method='bruteforce')
 
         pe1 = muniq[idx] * pytreegrav.PotentialTarget(np.atleast_2d(xuniq[idx]), cumul_pos, cumul_masses,
                                                 softening_target=np.atleast_1d(huniq[idx]), softening_source=cumul_soft,
@@ -183,10 +255,14 @@ def get_gas_mass_bound(sys1, xuniq, muniq, huniq, accel_gas, G=GN, cutoff=0.1, n
     return halo_mass, d_max
 
 
+
 parser = argparse.ArgumentParser(description="Parse starforge snapshot, and get multiple data.")
 parser.add_argument("snap", help="Index of snapshot to read")
 parser.add_argument("--non_pair", action="store_true", help="Flag to turn on non-pairwise algorithm")
+parser.add_argument("--compress", action="store_true", help="Filter out compressive tidal forces")
+parser.add_argument("--tides_factor", type=float, default=1.0, help="Prefactor for check of tidal criterion (1)")
 parser.add_argument("--cutoff", type=float, default=0.1, help="Outer cutoff to look for bound gas (0.1 pc)")
+
 
 args = parser.parse_args()
 
@@ -224,30 +300,20 @@ partpos = partpos.astype(np.float64)
 partmasses = partmasses.astype(np.float64)
 partsink = partsink.astype(np.float64)
 
-# bin_smas = np.concatenate(np.array([ss.orbits[:, 0] for ss in cl.systems], dtype=object)[sys_mult == 2])
-
-# halo_masses_bin = np.zeros(len(systems1))
-# max_dist_bin = np.zeros(len(systems1))
-# ids1 = np.zeros(len(halo_masses_bin))
-# ids2 = np.zeros(len(halo_masses_bin))
-# for ii, pp in enumerate(systems1):
-#     if sys_mult[ii] != 2:
-#         continue
-#     halo_masses_bin[ii], max_dist_bin[ii] = get_gas_mass_bound(systems1[ii], xuniq, muniq, huniq, accel_gas, G=GN, cutoff=cutoff,
-#                                              non_pair=non_pair)
-#     ids1[ii], ids2[ii] = systems1[ii].ids
-# #
-# np.savetxt("halo_masses_bin_{0}_np{1}_c{2}".format(snap_idx, non_pair, cutoff),
-#            np.transpose((halo_masses_bin, ids1, ids2, max_dist_bin)))
-
 halo_masses_sing = np.zeros(len(partpos))
 max_dist_sing = np.zeros(len(partpos))
 ids_sing = np.zeros(len(partpos))
 for ii, pp in enumerate(partpos):
     sys_tmp = find_multiples_new2.system(partpos[ii], partvels[ii], partmasses[ii], partsink[ii], partids[ii], accel_stars[ii], 0)
-    halo_masses_sing[ii], max_dist_sing[ii] = get_gas_mass_bound(sys_tmp, xuniq, muniq, huniq, accel_gas, G=GN, cutoff=cutoff, non_pair=non_pair)
+    halo_masses_sing[ii], max_dist_sing[ii], bound_index = get_gas_mass_bound_refactor(sys_tmp, (xuniq, vuniq, muniq, huniq, uuniq, accel_gas),
+                                                                                        G=GN, cutoff=cutoff, non_pair=non_pair,
+                                                                                        compress=args.compress, tides_factor=args.tides_factor)
     ids_sing[ii] = partids[ii]
+    output_file = "bound_stars_{0}_np{1}_c{2}_comp{3}_tf{4}_{5}".format(snap_idx, non_pair, cutoff, args.compress,
+                                                                     args.tides_factor, ii)
+    np.savetxt(output_file, bound_index)
 
-np.savetxt("tmp_halo_masses_sing_{0}_np{1}_c{2}".format(snap_idx, non_pair, cutoff),
-           np.transpose((halo_masses_sing, ids_sing, max_dist_sing)))
+output_file ="tmp_halo_masses_sing_{0}_np{1}_c{2}_comp{3}_tf{4}".format(snap_idx, non_pair, cutoff, args.compress,
+                                                                     args.tides_factor)
+np.savetxt(output_file, np.transpose((halo_masses_sing, ids_sing, max_dist_sing)))
 
