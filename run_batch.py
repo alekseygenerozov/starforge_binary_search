12279ce8f421658ca40ae62da382927337679ea7
Aleksey Generozov <aleksey.generozov@gmail.com>
2023-08-03 05:21:15 -0500

    run_batch:Adjusting scripts (eg python->python3)

--- run_batch.py
+++ run_batch.py
@@ -10,21 +10,19 @@ def bash_command(cmd, **kwargs):
 snap_base = "/scratch3/03532/mgrudic/STARFORGE_RT/STARFORGE_v1.1/M2e4_R10/M2e4_R10_S0_T1_B0.1_Res271_n2_sol0.5_42/output/snapshot"
 start = int(sys.argv[1]) * 10
 end = (int(sys.argv[1]) + 1) * 10
-# end = start + 1
 ##FOR TESTING
 # start = 250
 # end = 251
 # snap_base = "snapshot"
-# print(start, end)
 for ii in range(start, end, 1):
-	bash_command("python find_multiples_new2.py --halo_mass_file M2e4halo_masses_sing_\
+	bash_command("python3 find_multiples_new2.py --halo_mass_file M2e4halo_masses_sing_\
 npTrue_c0.5 --ngrid 4 --snap_base {0} {1} --tides_factor {2}".format(snap_base, ii, 8.0))
-	bash_command("python find_multiples_new2.py --halo_mass_file M2e4halo_masses_sing_\
+	bash_command("python3 find_multiples_new2.py --halo_mass_file M2e4halo_masses_sing_\
 npTrue_c0.5 --compress --ngrid 4 --snap_base {0} {1} --tides_factor {2}".format(snap_base, ii, 8.0))
-	bash_command("python find_multiples_new2.py --halo_mass_file M2e4halo_masses_sing_\
+	bash_command("python3 find_multiples_new2.py --halo_mass_file M2e4halo_masses_sing_\
 npTrue_c0.5 --ngrid 4 --snap_base {0} {1} --tides_factor {2}".format(snap_base, ii, 1.0))
 
-	bash_command("python find_multiples_new2.py --ngrid 4 --snap_base {0} {1} --tides_factor {2} ".format(snap_base, ii, 8.0))
-	bash_command("python find_multiples_new2.py --compress --ngrid 4 --snap_base {0} {1} --tides_factor {2}".format(snap_base, ii, 8.0))
-	bash_command("python find_multiples_new2.py --ngrid 4 --snap_base {0} {1} --tides_factor {2} ".format(snap_base, ii, 1.0))
+	bash_command("python3 find_multiples_new2.py --ngrid 4 --snap_base {0} {1} --tides_factor {2} ".format(snap_base, ii, 8.0))
+	bash_command("python3 find_multiples_new2.py --compress --ngrid 4 --snap_base {0} {1} --tides_factor {2}".format(snap_base, ii, 8.0))
+	bash_command("python3 find_multiples_new2.py --ngrid 4 --snap_base {0} {1} --tides_factor {2} ".format(snap_base, ii, 1.0))
 
--- run_batch.sh
+++ run_batch.sh
@@ -1,15 +1,28 @@
 #!/bin/bash 
 
-#SBATCH -J gas_halo
-#SBATCH --output=job.%J.out.txt
-#SBATCH --error=job.%J.err.txt
-#SBATCH --partition=all
-#SBATCH --nodes=1
-#SBATCH --ntasks=NTASKS
 
-#SBATCH --time=2-00:00:00
+#!/bin/sh
+#SBATCH -N 1 # Number of nodes
+#SBATCH -n 10 # Number of cores
+#SBATCH -p small # Queue
+#SBATCH -J R10_bin # Job name
+#SBATCH -o bin_%j.o # Name of stdout output file 
+#SBATCH -e bin_%j.e # Name of stderr error file 
+#SBATCH -A AST21002
+#SBATCH -t 48:00:00 # time in hours:minutes:sec ibrun ./hyperion input output
 
-python gas_batch.py $SLURM_ARRAY_TASK_ID
+module load hdf5
+module unload impi
+module load python3/3.9.2
+
+module load launcher
+
+export LAUNCHER_WORKDIR=/work2/09543/aleksey2/frontera/
+export LAUNCHER_JOB_FILE=run_batch_aux
+
+${LAUNCHER_DIR}/paramrun
+
+
     
 
 
