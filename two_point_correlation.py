3eec4e3fb30562e8321b179e04874c3800341ff1
Aleksey Generozov <aleksey.generozov@gmail.com>
2024-04-03 16:16:43 +0300

    Cleaning up two-point code.

--- two_point_correlation.py
+++ two_point_correlation.py
@@ -43,47 +43,23 @@ def get_two_point(d_pos, control_pos, absc):
 
     return two_point
 
-ncontrol = 3000
-control_pos = np.random.uniform(0, 400, ncontrol)
-control_pos.shape = (-1, 3)
-control_pos_og = np.copy(control_pos)
-thetas = np.arccos(np.random.uniform(-1, 1, len(control_pos_og)))
-phis = np.random.uniform(0, 2. * np.pi, len(control_pos_og))
-# control_pos_end = control_pos_og + 3.5 * np.transpose((np.cos(phis), np.sin(phis)))
-rs = np.random.normal(loc = 10.5, scale=0.5, size=len(control_pos_og))
-control_pos_end = control_pos_og +  np.transpose((rs * np.cos(phis) * np.sin(thetas), rs * np.sin(phis) * np.sin(thetas), rs * np.cos(thetas)))
-# control_pos_end = control_pos_og +  np.transpose((rs * np.cos(phis), rs * np.sin(phis)))
-
-dat = np.vstack((control_pos, control_pos_end))
-# np.savetxt("twod_test", control_pos)
 
 
 # ##Reading in data file -- array of positions.
-# dat = np.genfromtxt(sys.argv[1])
+dat = np.genfromtxt(sys.argv[1])
 # ##Ensure that the data is centered
 dat = dat - np.median(dat, axis=0)
-##Min and Max separation -- currently passed as cmd line arguments
+##Min and Max separation of interest -- currently passed as cmd line arguments
 dmin = float(sys.argv[2])
 dmax = float(sys.argv[3])
 ##Number of bins
 nbins = 100
 ##Bins to use.
 absc = np.linspace(dmin, dmax, nbins)
+##Generating the control sample -- may need to think about size more
+ncontrol = int((3. * dmax / absc[1])**1.5 * 10**.5 * 3)
+ncontrol = ncontrol // dat.shape[1] * dat.shape[1]
+control_pos = np.random.uniform(-1.5 * dmax, 1.5 * dmax, ncontrol)
+control_pos.shape = (-1, dat.shape[1])
+np.savez("two_point_func", absc, get_two_point(dat, control_pos, absc))
 
-fig, ax = plt.subplots()
-ax.set_xscale('log')
-fs = [16.0, 32.0]
-col = 0
-ls = ['-', '--']
-for fidx,ff in enumerate(fs):
-    ##Control sample -- also choose uniformly
-    ncontrol2 =  int(ff * ncontrol)
-    control_pos = np.random.uniform(-1.5 * dmax, 1.5 * dmax, ncontrol2)
-    control_pos.shape = (-1, dat.shape[1])
-
-    # np.savez("two_point_func", absc, get_two_point(dat, control_pos, absc))
-    absc_cen = 0.5 * (absc[:-1] + absc[1:])
-    plt.plot(absc_cen, get_two_point(dat, control_pos, absc), color=str(col), label=str(ff), linestyle=ls[fidx])
-    col+=0.5
-ax.legend()
-plt.show()
