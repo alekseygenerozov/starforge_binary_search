6b12b733123e2cb1ac13e982667547756b401fc2
Aleksey Generozov <aleksey.generozov@gmail.com>
2024-06-25 14:51:10 +0300

    Fixing typo

--- run_batch_halo_only_par.py
+++ run_batch_halo_only_par.py
@@ -14,7 +14,7 @@ snaps = glob.glob(snap_base + "*hdf5")
 
 start = int(sys.argv[1])
 end = int(sys.argv[2])
-if interval < 0:
+if end < 0:
 	interval = len(snaps) - 1
 for ii in range(start, end + 1, 1):
 	bash_command(f"python3 halo_masses_single_double_par.py --non_pair --tides_factor {sys.argv[3]} --snap_base {snap_base}  {ii}")
