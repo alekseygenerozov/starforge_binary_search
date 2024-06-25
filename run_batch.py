e042a999dffdaa2b880c2ff711026a8232754a6e
Aleksey Generozov <aleksey.generozov@gmail.com>
2024-06-25 14:09:11 +0300

    run_batch.py: Streamlining and changing ngrid.

--- run_batch.py
+++ run_batch.py
@@ -10,23 +10,9 @@ def bash_command(cmd, **kwargs):
 with open("data_loc", "r") as ff:
 	snap_base = ff.read()
 start = int(sys.argv[1])
-interval = 20
-if len(sys.argv)==3:
-        interval = int(sys.argv[2])
+interval = int(sys.argv[2])
 end = start + interval 
 print(start, end)
-##FOR TESTING
-# start = 250
-# end = 251
 # snap_base = "snapshot"
 for ii in range(start, end, 1):
-        bash_command("python3 find_multiples_new2.py --halo_mass_file M2e4halo_masses_sing_npTrue_c0.5 --ngrid 4 --snap_base {0} {1} --tides_factor {2}".format(snap_base, ii, 8.0))
-#         bash_command("python3 find_multiples_new2.py --halo_mass_file M2e4halo_masses_sing_\
-# npTrue_c0.5 --compress --ngrid 8 --snap_base {0} {1} --tides_factor {2}".format(snap_base, ii, 8.0))
-#         bash_command("python3 find_multiples_new2.py --halo_mass_file M2e4halo_masses_sing_\
-# npTrue_c0.5 --ngrid 8 --snap_base {0} {1} --tides_factor {2}".format(snap_base, ii, 1.0))
-#
-#         bash_command("python3 find_multiples_new2.py --ngrid 8 --snap_base {0} {1} --tides_factor {2} ".format(snap_base, ii, 8.0))
-#         bash_command("python3 find_multiples_new2.py --compress --ngrid 8 --snap_base {0} {1} --tides_factor {2}".format(snap_base, ii, 8.0))
-#         bash_command("python3 find_multiples_new2.py --ngrid 8 --snap_base {0} {1} --tides_factor {2} ".format(snap_base, ii, 1.0))
-#
+        bash_command(f"python3 find_multiples_new2.py --halo_mass_file halo_masses/M2e4halo_masses_sing_npTrue_c0.5 --ngrid 1 --snap_base {snap_base} {ii} --tides_factor {sys.argv[3]}")
